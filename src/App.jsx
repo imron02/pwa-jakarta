adb95f2e878b34520148ad263399b6548c51cb99
Imron Rosdiana <imron.r@meteor.id>
2018-11-17 14:13:03 +0700

    Update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import NormalLoginForm from './containers/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
+import NormalLoginForm from './containers/Login';
+import RegisterForm from './containers/Register';
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={NormalLoginForm} />
-      <Route path="/topics" component={Topics} />
+      <Route exact path="/register" component={RegisterForm} />
     </div>
   </Router>
 );
-
-Topic.propTypes = {
-  match: PropTypes.objectOf(PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.string
-  ])).isRequired
-};
-
-Topics.propTypes = {
-  match: PropTypes.objectOf(PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.string
-  ])).isRequired
-};
 
 export default App;
