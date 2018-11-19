da5814d3696b9573528a48a77e442a24bc4485d0
Imron Rosdiana <imron.r@meteor.id>
2018-11-19 22:18:55 +0700

    Add constant file

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyBzPH5FtH-nbaWARxBVbXINVAZpgTzeUPs',
+  authDomain: 'pwa-jakarta.firebaseapp.com',
+  databaseURL: 'https://pwa-jakarta.firebaseio.com',
+  projectId: 'pwa-jakarta',
+  storageBucket: 'pwa-jakarta.appspot.com',
+  messagingSenderId: '284832732690'
+};
+const SECRET_KEY = 'pw4-j4k4rt4-s3cr3t';
+
+export {
+  FIREBASE_CONFIG,
+  SECRET_KEY
+};
