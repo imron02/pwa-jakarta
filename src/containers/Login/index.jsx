c2bbf343d3aeca400fe88b713927ee264d29a558
imron02 <imron.r@meteor.id>
2018-12-06 11:26:42 +0700

    Add logic for handle user log in

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -3,30 +3,67 @@ import {
   Form,
   Icon,
   Input,
-  Button
+  Button,
+  notification
 } from 'antd';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import { firebase } from '../../utils/firebase';
+import { ERROR_CODE } from '../../utils/constants';
 import './index.scss';
 
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
-  handleSubmit = () => { }
+  state = {
+    onSubmit: false
+  };
+
+  onSubmit = (e) => {
+    const { form, history } = this.props;
+    e.preventDefault();
+
+    this.setState({ onSubmit: true });
+    form.validateFieldsAndScroll(async (formError, { email, password }) => {
+      if (!formError) {
+        try {
+          const { user } = await firebase.auth().signInWithEmailAndPassword(email, password);
+          this.setState({ onSubmit: false });
+          if (user.emailVerified) {
+            history.push('/register');
+          } else {
+            notification.warning({
+              message: 'Warning!',
+              description: 'Silahkan cek E-mail dan verifikasi akun kamu'
+            });
+          }
+        } catch ({ code, message }) {
+          if (code === ERROR_CODE['auth/user-not-found']) {
+            notification.warning({
+              message: 'Warning!',
+              description: 'Pengguna tidak ditemukan, mohon periksa E-mail kamu'
+            });
+          }
+          this.setState({ onSubmit: false });
+        }
+      }
+    });
+  }
 
   render() {
     const { form: { getFieldDecorator } } = this.props;
+    const { onSubmit } = this.state;
 
     return (
       <div id="form-login">
-        <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form onSubmit={this.onSubmit} className="login-form">
           <div id="form-input-login">
             <FormItem>
               {getFieldDecorator('email', {
                 rules: [
-                  { required: true, message: 'Please input your E-mail!' },
-                  { type: 'email', message: 'The input is not valid E-mail!' }
+                  { required: true, message: 'Silahkan masukkan E-mail kamu!' },
+                  { type: 'email', message: 'Yang kamu masukkan bukan E-mail yang valid!' }
                 ]
               })(
                 <Input
@@ -39,7 +76,10 @@ class NormalLoginForm extends React.Component {
             </FormItem>
             <FormItem>
               {getFieldDecorator('password', {
-                rules: [{ required: true, message: 'Please input your password!' }]
+                rules: [
+                  { required: true, message: 'Silahkan masukkan password kamu!' },
+                  { min: 6 }
+                ]
               })(
                 <Input
                   prefix={<Icon type="lock" className="input-icon" />}
@@ -54,9 +94,10 @@ class NormalLoginForm extends React.Component {
           <FormItem>
             <Button
               type="primary"
-              htmlType="button"
+              htmlType="submit"
               className="login-form-button"
               size="large"
+              loading={onSubmit}
             >
               Log in
             </Button>
@@ -72,7 +113,8 @@ class NormalLoginForm extends React.Component {
 }
 
 NormalLoginForm.propTypes = {
-  form: PropTypes.objectOf(PropTypes.func).isRequired
+  form: PropTypes.objectOf(PropTypes.func).isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired
 };
 
 export default Form.create()(NormalLoginForm);
