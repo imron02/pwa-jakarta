b7ca452a3a28182cd4b3cfe7067996ca093aee00
imron02 <imron.r@meteor.id>
2018-12-07 14:17:32 +0700

    Add dashboard page

---
+++ src/containers/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  state = {}
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default Dashboard;
