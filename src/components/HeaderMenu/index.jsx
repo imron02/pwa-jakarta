f683f1efc58357aa7680ef4b8bb236b21af90a72
imron02 <imron.r@meteor.id>
2018-12-12 15:47:24 +0700

    Add logout function

--- src/components/HeaderMenu/index.jsx
+++ src/components/HeaderMenu/index.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
+import { Redirect } from 'react-router-dom';
 
 import userImage from '../../assets/images/user.png';
+import { firebase } from '../../utils/firebase';
 import './index.scss';
 
 const { Header } = Layout;
 const { SubMenu } = Menu;
 
 class HeaderMenu extends Component {
-  state = {}
+  state = {
+    isLogout: false
+  }
 
   renderSubMenu = () => (
     <div className="submenu-title-wrapper">
@@ -16,11 +20,32 @@ class HeaderMenu extends Component {
     </div>
   );
 
+  onLogout = (e) => {
+    if (Number(e.key) === 1) {
+      firebase.auth().signOut().then(() => {
+        this.setState({ isLogout: true });
+      }).catch(() => {
+        // An error happened.
+      });
+    }
+  }
+
   render() {
+    const { isLogout } = this.state;
+
+    if (isLogout) {
+      return <Redirect to="/login" />;
+    }
+
     return (
       <Header className="header">
         <div className="logo" />
-        <Menu mode="horizontal" theme="dark" className="header-menu">
+        <Menu
+          mode="horizontal"
+          theme="dark"
+          className="header-menu"
+          onClick={this.onLogout}
+        >
           <SubMenu title={this.renderSubMenu()}>
             <Menu.Item key="1">Log out</Menu.Item>
           </SubMenu>
