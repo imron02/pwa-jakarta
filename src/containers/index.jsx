139ab37bb3cba953cec96808c5cae50c7889a93a
imron02 <imron.r@meteor.id>
2018-12-13 14:16:16 +0700

    Add base layout

---
+++ src/containers/index.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import PropTypes from 'prop-types';
+
+import Menus from '../components/Menus';
+import HeaderMnu from '../components/HeaderMenu';
+import './index.scss';
+
+const { Content } = Layout;
+
+class BaseContainer extends Component {
+  state = {};
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <Layout>
+        <HeaderMnu />
+        <Layout>
+          <Menus />
+          <Layout className="base-container">
+            <Content className="content-layout">
+              {children}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+BaseContainer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+};
+
+export default BaseContainer;
