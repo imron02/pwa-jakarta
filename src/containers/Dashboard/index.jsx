86cbbfbf219f935d8ad37c16696f6754a6f361db
imron02 <imron.r@meteor.id>
2018-12-13 14:16:28 +0700

    Update dashboard with base layout

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
 
-import Menus from '../../components/Menus';
-import HeaderMnu from '../../components/HeaderMenu';
+import BaseContainer from '../index';
 import './index.scss';
-
-const { Content } = Layout;
 
 class Dashboard extends Component {
   state = {};
 
   render() {
     return (
-      <Layout>
-        <HeaderMnu />
-        <Layout>
-          <Menus />
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Content style={{
-              margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280
-            }}
-            >
-              Content
-            </Content>
-          </Layout>
-        </Layout>
-      </Layout>
+      <BaseContainer>
+        <h1>Selamat datang di aplikasi smart city Jakata</h1>
+      </BaseContainer>
     );
   }
 }
